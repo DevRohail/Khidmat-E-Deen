9e529cc7b91613efffa016e4819e4460c72bf9d8
Rohail Ahmed <ahmedrohail2010@gmail.com>
2024-01-07 12:17:52 +0000

    Add favicon and styling to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Add a click event listener to the button
+contact_button.addEventListener('click', () => {
+    // Open Gmail's compose window in a new tab with the "to" field filled in
+    window.open('https://mail.google.com/mail/u/0/#compose?to=ahmedrohail2010@gmail.com');
+    console.log("mail sent");
+});
+
