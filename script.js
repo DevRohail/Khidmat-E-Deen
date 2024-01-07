b209daf0279b476ae8a78873b8f3365d4a9b30e1
Rohail Ahmed <ahmedrohail2010@gmail.com>
2024-01-07 14:09:59 +0000

    Refactor contact section and update styles

--- script.js
+++
@@ -1,8 +0,0 @@
-
-// Add a click event listener to the button
-contact_button.addEventListener('click', () => {
-    // Open Gmail's compose window in a new tab with the "to" field filled in
-    window.open('./contact.html', '_blank');
-    console.log("mail sent");
-});
-
